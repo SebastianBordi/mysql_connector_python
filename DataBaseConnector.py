0c018233bbc72f3f2aadff60b263c1d3ad1d6687
Sebitronic <sebastianbordi@gmail.com>
2019-06-12 22:26:19 -0300

    Agregado de pueba de conexion

--- DataBaseConnector.py
+++ DataBaseConnector.py
@@ -3,4 +3,23 @@
 #importacion de la libreria 
 import mysql.connector 
 
+#Definicion de valores de conexion
+dbUser = "User"
+dbPass = "Password"
+dbHost = "127.0.0.1"
+dbPort = "3306"
+dbBase = "DataBase"
+dbTabl = "Table"
 
+#Testeo de conexion
+def TestConexion ():
+    try:
+        cnx = mysql.connector.connect(user='User',  password='Password', host='127.0.0.1', port=3306, database='DataBase')
+        query = str.format("SELECT * FROM Table LIMIT 1;")
+        crs = cnx.cursor()
+        cnx.commit()
+        cnx.close()
+        return True
+    except mysql.connector.Error as err:
+        print(err)
+        return False
